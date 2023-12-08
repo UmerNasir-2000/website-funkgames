fd0d2fb63d22d273c60144a2b1fb60b7270c0e05
umer-maqsad <122520233+umer-maqsad@users.noreply.github.com>
2023-12-08 22:51:19 +0500

    (feat): added color palette 🎨

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,10 +1,28 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
-export default extendTheme({
+const config: ThemeConfig = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
+};
+
+export default extendTheme({
+  config,
   fonts: {
     heading: '\'Raleway\', sans-serif',
     body: '\'Poppins\', sans-serif',
+  },
+  colors: {
+    gray: {
+      50: '#f9f9f9',
+      100: '#ededed',
+      200: '#d3d3d3',
+      300: '#b3b3b3',
+      400: '#a0a0a0',
+      500: '#898989',
+      600: '#6c6c6c',
+      700: '#202020',
+      800: '#121212',
+      900: '#111',
+    },
   },
 });
