ecc5f18c9824545d3195b8ce1073b1561441e954
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2023-12-09 13:27:34 +0500

    (feat): added logo 🦄

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { Heading, Text } from '@chakra-ui/react';
+import { Heading, Image, Text } from '@chakra-ui/react';
+import logo from './assets/logo.svg';
 
 export default function App(): JSX.Element {
   return (
     <>
+      <Image src={logo}/>
       <Heading>FUNK GAMES</Heading>
       <Text>Testing...</Text>
     </>
