6ee73b9e81e662a14d53e91876d7db0cb743116b
umer-maqsad <122520233+umer-maqsad@users.noreply.github.com>
2023-12-09 17:09:41 +0500

    (feat): added search field in navbar

--- src/components/core/navbar/index.tsx
+++ src/components/core/navbar/index.tsx
@@ -3,7 +3,11 @@ import {
   Container,
   HStack,
   Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
 } from '@chakra-ui/react';
+import { AiOutlineSearch } from 'react-icons/ai';
 import ThemeSwitcher from '../themeSwitcher';
 
 export default function Navbar(): JSX.Element {
@@ -14,6 +18,12 @@ export default function Navbar(): JSX.Element {
           <Heading cursor="pointer" fontFamily="cursive">
             Funk Games
           </Heading>
+          <InputGroup w="65%" display={{ sm: 'none', md: 'block' }}>
+            <InputLeftElement pointerEvents="none">
+              <AiOutlineSearch color="gray.300" />
+            </InputLeftElement>
+            <Input type="text" placeholder="Search Games By Title Or Genre..." paddingY={5} sx={{ borderRadius: '100px' }} />
+          </InputGroup>
           <ThemeSwitcher />
         </HStack>
       </Container>
