b2e98196b9640089c2a8b19da040ffb5a2adcedf
umer-maqsad <122520233+umer-maqsad@users.noreply.github.com>
2023-12-10 11:48:49 +0500

    (perf)(game-grid): used cropped images in game card 💯

--- src/features/game/_components/gameCard/index.tsx
+++ src/features/game/_components/gameCard/index.tsx
@@ -3,12 +3,14 @@ import {
 } from '@chakra-ui/react';
 import { type GameModel } from '../../hooks/useGame';
 
+const getCroppedImage = (url: string): string => `https://media.rawg.io/media/crop/600/400/games/${url.split('games/')?.[1]}`;
+
 type Props = GameModel;
 
 export default function GameCard({ background_image, name }: Props): JSX.Element {
   return (
     <Card borderRadius="lg" overflowX="hidden" cursor="pointer">
-      <Image src={background_image} alt={name} />
+      <Image src={getCroppedImage(background_image)} alt={name} />
       <CardBody>
         <Heading as="h2" size="md">{name}</Heading>
       </CardBody>
